7e1de7be510d62a5f19af7f9d346f3d9fe641060
Diosef Danilo <diosefdanilo@gmail.com>
2026-03-04 16:53:22 -0300

    add IMC

--- 01-Variaveis/script.js
+++ 01-Variaveis/script.js
@@ -1,43 +1,26 @@
+function exibirNome() {
+    alert("Seu nome");
+}
 
-        function Nome() {
-            alert("Meu nome é Danilo");
-        }
-        function Idade() {
-            alert("Minha idade é 27 anos");
-        }
-        function Altura() {
-            alert("Minha altura é 1,75 metros");
-        }
-        function Peso() {
-            alert("Meu peso é 70 quilos");
+function exibirProfissao() {
+    alert("Sua profissão");
+}
+
+function exibirAnosEstudo() {
+    alert("Tenho X anos de estudo");
+
+}
+return "Ola, meu nome é XXXXX, estudoprogramação há X anos e gosto de JS";
+
+function calcularIMC() {
+        var peso = parseFloat(document.getElementById('number1').value);
+        var altura = parseFloat(document.getElementById('number2').value);
+
+        if (isNaN(peso) || isNaN(altura) || altura <= 0) {
+            document.getElementById('resultado').textContent = 'Por favor, insira valores válidos para peso e altura.';
+            return;
         }
 
- function pegarValores() {
-        const num1 = parseFloat(document.getElementById('num1').value);
-        const num2 = parseFloat(document.getElementById('num2').value);
-        return { num1, num2 };
-    }   
-
-    function mostraraResultado(valor) {
-        document.getElementById("resultado").innerText = "Resultado: " + valor;
-    }
-    function somar() {
-        let { num1, num2 } = pegarValores();
-        mostraraResultado(num1 + num2);
-    }
-    function subtrair() {
-        let { num1, num2 } = pegarValores();
-        mostraraResultado(num1 - num2);
-    }
-    function multiplicar() {
-        let { num1, num2 } = pegarValores();
-        mostraraResultado(num1 * num2);
-    }
-    function dividir() {
-        let { num1, num2 } = pegarValores();
-        if (num2 !== 0) {
-            mostraraResultado(num1 / num2);
-        } else {
-            mostraraResultado("Erro: Divisão por zero")
-        }
+        var imc = peso / (altura * altura);
+        document.getElementById('resultado').textContent = 'Seu IMC é: ' + imc.toFixed(2);
     }
