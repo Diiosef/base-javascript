1eb636af841a903797d599de55d1f985080221a1
Diosef Danilo <diosefdanilo@gmail.com>
2026-03-04 16:57:26 -0300

    I

--- 01-Variaveis/script.js
+++ 01-Variaveis/script.js
@@ -8,19 +8,23 @@ function exibirProfissao() {
 
 function exibirAnosEstudo() {
     alert("Tenho X anos de estudo");
-
 }
-return "Ola, meu nome é XXXXX, estudoprogramação há X anos e gosto de JS";
+
+function apresentar() {
+    alert("Olá, meu nome é Danilo, estudo programação há X anos e gosto de JS");
+}
 
 function calcularIMC() {
-        var peso = parseFloat(document.getElementById('number1').value);
-        var altura = parseFloat(document.getElementById('number2').value);
+    var peso = parseFloat(document.getElementById('number1').value);
+    var altura = parseFloat(document.getElementById('number2').value);
 
-        if (isNaN(peso) || isNaN(altura) || altura <= 0) {
-            document.getElementById('resultado').textContent = 'Por favor, insira valores válidos para peso e altura.';
-            return;
-        }
-
-        var imc = peso / (altura * altura);
-        document.getElementById('resultado').textContent = 'Seu IMC é: ' + imc.toFixed(2);
+    if (isNaN(peso) || isNaN(altura) || altura <= 0) {
+        document.getElementById('resultado').textContent =
+        'Por favor, insira valores válidos para peso e altura.';
+        return;
     }
+
+    var imc = peso / (altura * altura);
+    document.getElementById('resultado').textContent =
+    'Seu IMC é: ' + imc.toFixed(2);
+}
